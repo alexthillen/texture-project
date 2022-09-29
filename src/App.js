0ca2377553c024ddc202f1ea25838d5329e29962
Alex Thillen <alexthillen852@gmail.com>
2022-09-29 12:25:53 +0200

    add pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { lazy } from 'react';
+import { Routes, BrowserRouter, Route, Navigate } from 'react-router-dom';
 
 // Importing bootstrap styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import React from 'react';
 
 const { PUBLIC_URL } = process.env;
 
@@ -20,7 +22,19 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter basename={PUBLIC_URL}>
-      
+        <Routes>
+          <Route path="/" element={<Navigate to="/de/" replace />}/>
+          <Route path="/de/" element={<Index lang="de" />}/>
+          <Route path="/fr/" element={<Index lang="fr" />}/>
+          <Route path="/de/contact" element={<Contact lang="de" />}/>
+          <Route path="/fr/contact" element={<Contact lang="fr" />}/>
+          <Route path="/de/1" element={<OfferOne lang="de" />}/>
+          <Route path="/fr/1" element={<OfferOne lang="fr" />}/>
+          <Route path="/de/2" element={<OfferTwo lang="de" />}/>
+          <Route path="/fr/2" element={<OfferTwo lang="fr" />}/>
+          <Route path="/de/about" element={<About lang="de" />}/>
+          <Route path="/fr/about" element={<About lang="fr" />}/>
+        </Routes>
     </BrowserRouter>
     </div>
   );
---
+++ src/pages/OfferTwo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function OfferTwo(props) {
+  return (
+    <div>OfferTwo</div>
+  )
+}
