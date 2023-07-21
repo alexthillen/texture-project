b6275efdbe4d50008fdfee8018d17c2c8fa8cf2f
Alex Thillen <alexthillen852@gmail.com>
2023-07-21 22:54:44 +0200

    lint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { lazy } from 'react';
 import { Routes, HashRouter, Route, Navigate } from 'react-router-dom';
 import Index from './pages/Index';
 import Contact from 'pages/Contact';
@@ -26,20 +25,20 @@ function App() {
     <div className="App">
       <HashRouter basename='/'>
         <Routes>
-          <Route path="" element={<Navigate to="/fr" replace={true}/>}/>
-          <Route path="/" element={<Navigate to="/de" replace={true}/>}/>
-          <Route path="/de" element={<Index lang="de" />}/>
-          <Route path="/fr" element={<Index lang="fr" />}/>
-          <Route path="/de/contact" element={<Contact lang="de" />}/>
-          <Route path="/fr/contact" element={<Contact lang="fr" />}/>
-          <Route path="/de/1" element={<OfferOne lang="de" />}/>
-          <Route path="/fr/1" element={<OfferOne lang="fr" />}/>
-          <Route path="/de/2" element={<OfferTwo lang="de" />}/>
-          <Route path="/fr/2" element={<OfferTwo lang="fr" />}/>
-          <Route path="/de/about" element={<About lang="de" />}/>
-          <Route path="/fr/about" element={<About lang="fr" />}/>
+          <Route path="" element={<Navigate to="/fr" replace={true} />} />
+          <Route path="/" element={<Navigate to="/de" replace={true} />} />
+          <Route path="/de" element={<Index lang="de" />} />
+          <Route path="/fr" element={<Index lang="fr" />} />
+          <Route path="/de/contact" element={<Contact lang="de" />} />
+          <Route path="/fr/contact" element={<Contact lang="fr" />} />
+          <Route path="/de/1" element={<OfferOne lang="de" />} />
+          <Route path="/fr/1" element={<OfferOne lang="fr" />} />
+          <Route path="/de/2" element={<OfferTwo lang="de" />} />
+          <Route path="/fr/2" element={<OfferTwo lang="fr" />} />
+          <Route path="/de/about" element={<About lang="de" />} />
+          <Route path="/fr/about" element={<About lang="fr" />} />
         </Routes>
-    </HashRouter>
+      </HashRouter>
     </div>
   );
 }
